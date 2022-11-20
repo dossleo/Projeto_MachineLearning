2aa1fd6cb7eeebc9beedab82ea2ef256c1772d7e
pepinheiro <pedropinheiro@rodaconveniencia.com.br>
2022-11-20 14:01:49 -0300

    Arquitectural changes

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+mapped_databases = [
+    # '1 - Three Baseline Conditions',
+    '2 - Three Outer Race Fault Conditions',
+    # '3 - Seven More Outer Race Fault Conditions',
+    # '4 - Seven Inner Race Fault Conditions',
+    # '5 - Analyses',
+    # '6 - Real World Examples'
+]
