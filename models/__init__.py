8295d4a463a68ffd607fa48c2b2b66d4c0e5bdd1
dossleo <leonardo.pacheco.santos@outlook.com>
2022-11-20 19:39:30 -0300

    mapped_database atualizada

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,9 @@
 # General Parameters
 mapped_databases = [
-    # '1 - Three Baseline Conditions',
+    '1 - Three Baseline Conditions',
     '2 - Three Outer Race Fault Conditions',
-    # '3 - Seven More Outer Race Fault Conditions',
-    # '4 - Seven Inner Race Fault Conditions',
-    # '5 - Analyses',
-    # '6 - Real World Examples'
+    '3 - Seven More Outer Race Fault Conditions',
+    '4 - Seven Inner Race Fault Conditions'
 ]
 
 # General Machine Learning Parameters
