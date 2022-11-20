34e452aa58311972e5cf112c26a3456ed11cc227
pepinheiro <pedropinheiro@rodaconveniencia.com.br>
2022-11-20 18:16:52 -0300

    working project

--- main.py
+++ main.py
@@ -13,16 +13,6 @@ pretty.install()
 def main(dataframe:pd.DataFrame = pd.DataFrame()):
     df_data = dataframe
 
-    if len(dataframe) == 0:
-        file_name = 'OuterRaceFault_1.mat'
-        folder_name = '2 - Three Outer Race Fault Conditions'
-
-        # Lê o arquivo
-        mat_data = data_handle.Read(file_name, folder_name).data
-        data = mat_data.get("bearing")[0][0][2][:,0]
-        df_data = pd.DataFrame({"data":data,"Defeito":"outer race"})
-
-
     x_columns = ['maximum', 'minimum', 'mean', 'standard_deviation', 'rms', 'skewness', 'kurtosis', 'form_factor', 'crest_factor']
     y_column = 'fault'
     score = {}
@@ -31,21 +21,20 @@ def main(dataframe:pd.DataFrame = pd.DataFrame()):
     classifier = ml_functions.Classifier(data = df_data, x_columns=x_columns, y_column=y_column, classifier=RandomForestClassifier, random_state = seed)
     classifier.run()
     name = classifier.classifier.__class__.__name__
-    score[f"Classification Score {name}%"] = round(classifier.score * 100,2)
+    score[f"Classification Score {name}"] = round(classifier.score * 100,2)
 
     classifier = ml_functions.Classifier(data = df_data, x_columns=x_columns, y_column=y_column, classifier=KNeighborsClassifier)
     classifier.run()
     name = classifier.classifier.__class__.__name__
-    score[f"Classification Score {name}%"] = round(classifier.score * 100,2)
+    score[f"Classification Score {name}"] = round(classifier.score * 100,2)
 
     classifier = ml_functions.Classifier(data = df_data, x_columns=x_columns, y_column=y_column, classifier=SVC, random_state = seed)
     classifier.run()
     name = classifier.classifier.__class__.__name__
-    score[f"Classification Score {name}%"] = round(classifier.score * 100,2)
+    score[f"Classification Score {name}"] = round(classifier.score * 100,2)
 
     return score
 
 if __name__ == "__main__":
     data = get_data(95)
     print(main(data))
-        
