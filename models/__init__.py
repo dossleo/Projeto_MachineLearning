0478c32d345163b65d3a67736a532d98c9a7d1f6
pepinheiro <pedropinheiro@rodaconveniencia.com.br>
2022-11-20 21:35:36 -0300

    Implementing new global variables

--- models/__init__.py
+++ models/__init__.py
@@ -6,6 +6,17 @@ mapped_databases = {
         '4 - Seven Inner Race Fault Conditions': 'inner race'
     }
 
+faults = ["normal","outer race","inner race"]
+
+# Data Parameters
+frequency_rate_dict = {
+    "normal":97656,
+    "outer race": 97656,
+    "inner race": 48828
+}
+time_window = 1
+overlap = 80
+
 # General Machine Learning Parameters
 seed = 30
 test_size = 0.25
